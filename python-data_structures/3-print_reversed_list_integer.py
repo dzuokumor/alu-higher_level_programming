7f163f2877819e3a0b7272f3b2a4ddec26e84712
dzuokumor <d.zuokumor@alustudent.com>
2023-11-09 08:37:00 -0800

    Data Structures

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,9 +2,10 @@
 
 def print_reversed_list_integer(my_list=[]):
     for i in range(len(my_list) - 1, -1, -1):
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
 
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
     print_reversed_list_integer(my_list)
+
 ~                                     
