ef724b3449cd9e2a4aa9ec2d21eacab79c5464a6
dzuokumor <d.zuokumor@alustudent.com>
2023-12-01 14:02:12 -0800

    Networ

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+"""Fetches https://alu-intranet.hbtn.io/status."""
 import urllib.request
-import urllib.error
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    try:
-        request = urllib.request.Request(url)
-        with urllib.request.urlopen(request) as response:
-            body = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
-            print("\t- utf8 content: {}".format(body.decode("utf-8")))
-    except urllib.error.HTTPError as e:
-        print("Error accessing {}: {}".format(url, e))
+    url = "https://alu-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
